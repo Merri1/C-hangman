6116ff14c00d2c1aa88cb3d9d88bad5f751f3aaa
Merri1 <m121212@live.ie>
2023-04-17 11:40:57 +0100

    First Draft with partial functionality

---
+++ hangman.h
@@ -0,0 +1,86 @@
+#ifndef HANGMAN_H
+#define HANGMAN_H
+
+ #include <sys/types.h>
+ #include <sys/socket.h>
+ #include <netinet/in.h>
+ #include <stdio.h>
+ #include <syslog.h>
+ #include <signal.h>
+ #include <errno.h>
+ #include <stdlib.h>
+ #include <string.h>
+ #include <unistd.h>
+ #include <netdb.h>
+ 
+
+
+ extern time_t time ();
+
+ int maxlives = 12;
+ char *word [] = {
+ # include "words"
+ };
+ # define NUM_OF_WORDS (sizeof (word) / sizeof (word [0]))
+ # define MAXLEN 80 /* Maximum size in the world of Any string */
+ /*# define HANGMAN_TCP_PORT 1066*/
+
+play_hangman (int in, int out)
+ {
+ 	char * whole_word, part_word [MAXLEN],
+ 	guess[MAXLEN], outbuf [MAXLEN];
+
+ 	int lives = maxlives;
+ 	int game_state = 'I';//I = Incomplete
+ 	int i, good_guess, word_length;
+ 	char hostname[MAXLEN];
+
+ 	gethostname (hostname, MAXLEN);
+ 	sprintf(outbuf, "Playing hangman on host% s: \n \n", hostname);
+ 	write(out, outbuf, strlen (outbuf));
+ 	
+ 	
+
+ 	/* Pick a word at random from the list */
+ 	whole_word = word[rand() % NUM_OF_WORDS];
+ 	word_length = strlen(whole_word);
+ 	syslog (LOG_USER | LOG_INFO, "server chose hangman word %s", whole_word);
+
+ 	/* No letters are guessed Initially */
+ 	for (i = 0; i <word_length; i++)
+ 		part_word[i]='-';
+ 	
+	part_word[i] = '\0';
+
+ 	sprintf (outbuf, "%s %d \n", part_word, lives);
+ 	write (out, outbuf, strlen(outbuf));
+
+ 	while (game_state == 'I')
+ 	/* Get a letter from player guess */
+ 	{
+		while (read (in, guess, MAXLEN) <0) {
+ 			if (errno != EINTR)
+ 				exit (4);
+ 			printf ("re-read the startin \n");
+ 			} /* Re-start read () if interrupted by signal */
+ 	good_guess = 0;
+ 	for (i = 0; i <word_length; i++) {
+ 		if (guess [0] == whole_word [i]) {
+ 		good_guess = 1;
+ 		part_word [i] = whole_word [i];
+ 		}
+ 	}
+ 	if (! good_guess) lives--;
+ 	if (strcmp (whole_word, part_word) == 0)
+ 		game_state = 'W'; /* W ==> User Won */
+ 		
+ 	else if (lives == 0) {
+ 		game_state = 'L'; /* L ==> User Lost */
+ 		strcpy (part_word, whole_word); /* User Show the word */
+ 	}
+ 	sprintf (outbuf, "%s %d \n", part_word, lives);
+ 	write (out, outbuf, strlen (outbuf));
+ 	}
+ }
+ 	
+#endif
---
+++ hangserver_iter.c
@@ -0,0 +1,96 @@
+ /* Network server for hangman game */
+ /* File: hangserver.c */
+
+ #include <sys/types.h>
+ #include <sys/socket.h>
+ #include <netinet/in.h>
+ #include <stdio.h>
+ #include <syslog.h>
+ #include <signal.h>
+ #include <errno.h>
+ #include <stdlib.h>
+ #include <string.h>
+ #include <unistd.h>
+ #include <netdb.h>
+ #include "hangman.h"
+ 
+
+
+ extern time_t time ();
+
+ # define NUM_OF_WORDS (sizeof (word) / sizeof (word [0]))
+ # define MAXLEN 80 /* Maximum size in the world of Any string */
+ /*# define HANGMAN_TCP_PORT 1066*/
+
+ main()
+ {
+ 	int sock, fd, client_len, r, process_id;
+ 	struct addrinfo hints, *server;
+ 	struct sockaddr client;
+ 	
+ 	/*configuring the host*/
+ 	
+ 	printf("Configuring the host.....,\n");
+ 	memset(&hints, 0, sizeof(struct addrinfo) );
+ 	
+ 	hints.ai_family = AF_INET;
+ 	hints.ai_socktype = SOCK_STREAM;
+ 	
+ 	r=getaddrinfo(0, "8080", &hints, &server);
+ 	
+ 	if(r!=0)
+ 	{
+ 		perror("failed");
+ 		exit(1);
+ 	} 	
+ 	puts("host configured,\n");
+
+ 	srand ((int) time ((long *) 0)); /* randomize the seed */
+ 	
+ 	/*create the socket for the server*/
+ 	
+ 	printf("assign a socket");
+
+ 	sock = socket (server->ai_family, server->ai_socktype,server->ai_protocol);//0 or IPPROTO_TCP
+ 	if (sock ==-1) { //This error checking is the code Stevens wraps in his Socket Function etc
+ 		perror ("Erro creating server socket");
+ 		exit (1);
+ 	}
+ 	puts("Server socket created");
+
+ 	
+
+ 	if (bind(sock, server->ai_addr,server->ai_addrlen) <0) {
+ 		perror ("binding socket");
+	 	exit (2);
+ 	}
+	puts("Server socket is listening for incoming requests from Clients");
+	puts("Lets play hangman!");
+ 	listen (sock, 5);
+
+ 	while (1) {
+ 		client_len = sizeof (client);
+ 		/*accept function*/
+ 		puts("accepting new connection");
+ 		if ((fd = accept (sock,  &client, &client_len)) <0) {
+ 			perror ("accepting connection");
+ 			exit (3);
+ 		}
+ 		else {
+ 		
+ 		}
+ 		
+ 		process_id = fork();
+ 		
+ 		if(process_id == 0)
+ 		{
+ 			// Child process
+ 			printf("Created child process %d \n", getpid());
+ 			close(sock);
+ 			play_hangman (fd, fd);
+ 			exit(0);
+ 			
+ 			close (fd);
+ 		}
+ 	}
+ }
