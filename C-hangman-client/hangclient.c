61dbda5999ae5277994edda792aa21452650c23c
Merri1 <m121212@live.ie>
2023-04-21 11:34:30 +0100

    formatting

--- C-hangman-client/hangclient.c
+++ C-hangman-client/hangclient.c
@@ -1,91 +1,88 @@
 /* Hangclient.c - Client for hangman server.  */
 
- #include <stdio.h>
- #include <sys/types.h>
- #include <sys/socket.h>
- #include <netinet/in.h>
- #include <netdb.h>
+#include <stdio.h>
+#include <sys/types.h>
+#include <sys/socket.h>
+#include <netinet/in.h>
+#include <netdb.h>
 #include <stdlib.h>
- #include <string.h>
- #include <unistd.h>
- #include <netdb.h>
- 
- # define LINESIZE 80
- 
+#include <string.h>
+#include <unistd.h>
+#include <netdb.h>
 
- int main (int argc, char * argv [])
- {
- 	struct addrinfo hints, *server; /* Server's address  assembled here */
- 	
- 	int sock, count, r;
- 	 char i_line[LINESIZE];
- 	 char o_line[LINESIZE];
- 	char * server_name;
+# define LINESIZE 80
 
- 	/* Get server name from the command line.  If none, use 'localhost' 
 
- 	server_name = (argc = 1)?  argv [1]: "localhost";
- 	*/
- 	/*configuring the server address*/
- 	
- 	printf("Configuring the server.....,\n");
- 	memset(&hints, 0, sizeof(struct addrinfo) );
- 	
- 	hints.ai_family = AF_INET;
- 	hints.ai_socktype = SOCK_STREAM;
- 	
- 	r=getaddrinfo(0, "8080", &hints, &server);
- 	
- 	if(r!=0)
- 	{
- 		perror("failed");
- 		exit(1);
- 	} 	
- 	puts("server configured,\n");
+int main(int argc, char *argv[]) {
+    struct addrinfo hints, *server; /* Server's address  assembled here */
 
- 	/* Create the socket */
- 	/*create the socket for the server*/
- 	
- 	printf("assign client socket");
+    int sock, count, r;
+    char i_line[LINESIZE];
+    char o_line[LINESIZE];
+    char *server_name;
 
- 	sock = socket (server->ai_family, server->ai_socktype,server->ai_protocol);//0 or IPPROTO_TCP
- 	if (sock ==-1) { //This error checking is the code Stevens wraps in his Socket Function etc
- 		perror ("Erro creating server socket");
- 		exit (1);
- 	}
- 	puts("Client socket created");
- 	/*............................ */
-	
- 	
+    /* Get server name from the command line.  If none, use 'localhost'
 
- 	/* Set up the server's socket address, then connect */
+    server_name = (argc = 1)?  argv [1]: "localhost";
+    */
+    /*configuring the server address*/
 
- 	hints.ai_family = AF_INET;
- 	hints.ai_socktype = SOCK_STREAM;
- 	
- 	r=getaddrinfo(0, "8080", &hints, &server);
- 	
- 	if(r!=0)
- 	{
- 		perror("failed");
- 		exit(1);
- 	} 	
- 	puts("connecting .....,\n");
- 	/*server.sin_port = htons (HANGMAN_TCP_PORT);*/
+    printf("Configuring the server.....,\n");
+    memset(&hints, 0, sizeof(struct addrinfo));
 
- 	r = connect(sock, server->ai_addr, server->ai_addrlen); 
- 	freeaddrinfo(server);
- 	
- 	
- 	/*OK connected to the server.  
- Take a line from the server and show it, take a line and send the user input to the server. 
- Repeat until the server terminates the connection. */
+    hints.ai_family = AF_INET;
+    hints.ai_socktype = SOCK_STREAM;
 
- 	printf ("Connected to server% s \ n", server_name);
- 	while ((count = read (sock, i_line, LINESIZE)) > 0) {
- 		write (1, i_line, count);
- 		count = read (0, o_line, LINESIZE);//0 = STDIN
- 		write (sock, o_line, count);
- 		};
- 	
+    r = getaddrinfo(0, "8080", &hints, &server);
+
+    if (r != 0) {
+        perror("failed");
+        exit(1);
+    }
+    puts("server configured,\n");
+
+    /* Create the socket */
+    /*create the socket for the server*/
+
+    printf("assign client socket");
+
+    sock = socket(server->ai_family, server->ai_socktype, server->ai_protocol);//0 or IPPROTO_TCP
+    if (sock == -1) { //This error checking is the code Stevens wraps in his Socket Function etc
+        perror("Erro creating server socket");
+        exit(1);
+    }
+    puts("Client socket created");
+    /*............................ */
+
+
+
+    /* Set up the server's socket address, then connect */
+
+    hints.ai_family = AF_INET;
+    hints.ai_socktype = SOCK_STREAM;
+
+    r = getaddrinfo(0, "8080", &hints, &server);
+
+    if (r != 0) {
+        perror("failed");
+        exit(1);
+    }
+    puts("connecting .....,\n");
+    /*server.sin_port = htons (HANGMAN_TCP_PORT);*/
+
+    r = connect(sock, server->ai_addr, server->ai_addrlen);
+    freeaddrinfo(server);
+
+
+    /*OK connected to the server.
+Take a line from the server and show it, take a line and send the user input to the server.
+Repeat until the server terminates the connection. */
+
+    printf("Connected to server% s \ n", server_name);
+    while ((count = read(sock, i_line, LINESIZE)) > 0) {
+        write(1, i_line, count);
+        count = read(0, o_line, LINESIZE);//0 = STDIN
+        write(sock, o_line, count);
+    };
+
 }
--- C-hangman-server/cmake-build-debug/cmake_install.cmake
+++ C-hangman-server/cmake-build-debug/cmake_install.cmake
@@ -1,4 +1,4 @@
-# Install script for directory: /home/merri/CLionProjects/hangman/C-hangman-server
+# Install script for directory: /home/merri/CLionProjects/C-hangman/C-hangman-server
 
 # Set the install prefix
 if(NOT DEFINED CMAKE_INSTALL_PREFIX)
@@ -50,5 +50,5 @@ endif()
 
 string(REPLACE ";" "\n" CMAKE_INSTALL_MANIFEST_CONTENT
        "${CMAKE_INSTALL_MANIFEST_FILES}")
-file(WRITE "/home/merri/CLionProjects/hangman/C-hangman-server/cmake-build-debug/${CMAKE_INSTALL_MANIFEST}"
+file(WRITE "/home/merri/CLionProjects/C-hangman/C-hangman-server/cmake-build-debug/${CMAKE_INSTALL_MANIFEST}"
      "${CMAKE_INSTALL_MANIFEST_CONTENT}")
